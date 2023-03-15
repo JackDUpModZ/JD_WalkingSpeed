60f67e1c163711e6d20694d7315cc267d2b5337a
JackDUpModZ <83776161+JackDUpModZ@users.noreply.github.com>
2023-03-15 19:24:40 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+game 'gta5'
+lua54 'yes'
+
+server_scripts {
+	'server/server.lua'
+}
+
+client_scripts {
+	'client/client.lua'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua'
+}
